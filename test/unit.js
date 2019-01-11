db3034c9910e9d62677d85d28d779c6616e886c3
mestafor <mestalex@gmail.com>
2019-01-12 01:22:18 +0200

    Testing test

--- test/unit.js
+++ test/unit.js
@@ -32,25 +32,25 @@ unit['helpers.getANumber should return 1'] = (done) => {
 //   done();
 // };
 
-// Logs.list should callback an array and a false error
-unit['logs.list should callback a false error and an array of logs names'] = (done) => {
-  logs.list(true, (err, logFileNames) => {
-    assert.equal(err, false);
-    assert.ok(logFileNames instanceof Array);
-    assert.ok(logFileNames.length > 1);
-    done();
-  });
-};
+// // Logs.list should callback an array and a false error
+// unit['logs.list should callback a false error and an array of logs names'] = (done) => {
+//   logs.list(true, (err, logFileNames) => {
+//     assert.equal(err, false);
+//     assert.ok(logFileNames instanceof Array);
+//     assert.ok(logFileNames.length > 1);
+//     done();
+//   });
+// };
 
-// Logs truncate should not throw if the logId does not exist
-unit['logs.truncate should not throw if the logId does not exist. It should callback an error instead.'] = (done) => {
-  assert.doesNotThrow(() => {
-    logs.truncate('I do not exist', (err) => {
-      assert.ok(err);
-      done();
-    });
-  }, TypeError);
-};
+// // Logs truncate should not throw if the logId does not exist
+// unit['logs.truncate should not throw if the logId does not exist. It should callback an error instead.'] = (done) => {
+//   assert.doesNotThrow(() => {
+//     logs.truncate('I do not exist', (err) => {
+//       assert.ok(err);
+//       done();
+//     });
+//   }, TypeError);
+// };
 
 // // exampleDebuggingProblem.init should not throw (but it does)
 // unit['exampleDebuggingProblem.init should not throw but it does'] = (done) => {
